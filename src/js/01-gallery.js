e101caf39a4200e26e0b0308e5fa2f9fc4da6371
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-02-14 11:56:39 +0100

    add started files

---
+++ src/js/01-gallery.js
@@ -0,0 +1,8 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// // Change code below this line
+// import SimpleLightbox from 'simplelightbox';
+// // Додатковий імпорт стилів
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// console.log(galleryItems);
