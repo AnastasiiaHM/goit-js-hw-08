40c4a9f7a36e4aaf02e4e2841ecd1ee0608662d3
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-02-15 13:36:01 +0100

    add 2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// // Change code below this line
-// import SimpleLightbox from 'simplelightbox';
-// // Додатковий імпорт стилів
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// console.log(galleryItems);
+const wraperGalleryEl = document.querySelector('.gallery');
+
+function createGaleryItemsElem(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+    })
+    .join('');
+}
+
+const galleryItem = createGaleryItemsElem(galleryItems);
+wraperGalleryEl.insertAdjacentHTML('beforeend', galleryItem);
+let galleryImages = new SimpleLightbox('.gallery a', {
+  overlayOpacity: 0.4,
+  captions: true,
+  captionSelector: 'img',
+  captionsData: 'title',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  animationSpeed: 250,
+});
+galleryImages.on('error.simplelightbox', function (e) {
+  console.log(e);
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOKALSTOR = 'videoplayer-current-time';
+
+function saveCurrentTimeVideo({ seconds }) {
+  localStorage.setItem(LOKALSTOR, seconds);
+}
+player.on('timeupdate', throttle(saveCurrentTimeVideo, 1000));
+
+const currentTime = localStorage.getItem(LOKALSTOR);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
